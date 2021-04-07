e515a606712b2d745ed02c502af1e9cd0026d6fd
stevearchuleta <stevearchuleta@me.com>
2021-04-07 14:05:04 -0700

    push players into starters array and/or substitutes array

--- app.js
+++ app.js
@@ -1,56 +1,80 @@
-//==========
+//====
 // Import dependencies.
-//==========
+//====
 const Player = require('./player');
 const inquirer = require('inquirer');
 
+//====
+// ARRAYS
+//====
+var players = [];
+var starters = [];
+var substitutes = [];
+const questions = [
+    {
+        name: 'name',
+        message: "What is the player's name?"
+    },
+    {
+        name: 'position',
+        message: "What is the player's position?"
+    },
+    {
+        name: 'offense',
+        message: 'How many offense points does this player contribute (0-10)?',
+        validate: function(input){
+            if(isNaN(input) === false && parseInt(input) > 0 && parseInt(input) < 11){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        name: 'defense',
+        message: 'How many defensive points does this player contribute (0-10)?',
+        validate: function(input){
+            if(isNaN(input) === false && parseInt(input) > 0 && parseInt(input) < 11){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+];
 
 //==========
 // Prompt input using inquirer.
+// Recursively...
 // Instantiate a new Player() object.
 //==========
 function promptCreationOfPlayer() {
-    console.log('\n====\n NEW PLAYER \n====\n');
-    
-    const questions = [
-        {
-            name: 'name',
-            message: "What is the player's name?"
-        },
-        {
-            name: 'position',
-            message: "What is the player's position?"
-        },
-        {
-            name: 'offense',
-            message: 'How many offense points does this player contribute (0-10)?',
-            validate: function(input){
-                if(isNaN(input) === false && parseInt(input) > 0 && parseInt(input) < 11){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            name: 'defense',
-            message: 'How many defensive points does this player contribute (0-10)?',
-            validate: function(input){
-                if(isNaN(input) === false && parseInt(input) > 0 && parseInt(input) < 11){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-    ];
+    if(players.length < 3) {
 
-    inquirer.prompt(questions).then(function(answers){
-        const player = new Player(
-            answers.name,
-            answers.position,
-            parseInt(answers.offense),
-            parseInt(answers.defense)
-        );
-    })        
+        console.log('\n====\n NEW PLAYER \n====\n');
+        
+        inquirer.prompt(questions).then(function(answers){
+            const player = new Player(
+                answers.name,
+                answers.position,
+                parseInt(answers.offense),
+                parseInt(answers.defense)
+                );
+                if(starters.length < 2){
+                    starters.push(player);
+                } else {
+                    substitutes.push(player);
+                }
+                players.push(player);
+
+            promptCreationOfPlayer();     
+        });   
+        
+    } else {
+        console.log('\n====\n PLAYERS \n====\n', players);  
+        console.log('\n====\n STARTERS \n====\n', starters);  
+        console.log('\n====\n SUBSTITUTES \n====\n', substitutes);  
+    }
 }
+
+promptCreationOfPlayer()
